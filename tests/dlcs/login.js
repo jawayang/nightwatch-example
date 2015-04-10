77ad8acc661fe70f9c0e357882551ded1633197e
James Yang <james@cloud-interactive.com>
2015-04-10 12:22:36 +0800

    測試成功

--- tests/dlcs/login.js
+++ tests/dlcs/login.js
@@ -1,59 +1,81 @@
 module.exports = {
 
-  "第一步測試" : function (browser) {
-    browser
-      .resizeWindow(1920, 1080)
-      .saveScreenshot('./results/test.png')
-      // 開啟視窗
-      .url("https://microscope.paragon.dlcscpth.com/")
-      .waitForElementVisible('input[placeholder=Username]', 15000, '成功開啟視窗')
-      .setValue('input[placeholder=Username]', 'CLOUDPATH')
-      .setValue('input[placeholder=Password]', 'Cloud1111')
-      .execute(function(data){
-          $('#loginBtn').trigger('touchend');
-      })
-      .waitForElementVisible('.name.ellipsis',100000 ,'成功登入')
-      .assert.visible(".name.ellipsis")
-      .assert.containsText('.name.ellipsis','Cloud Pathologist');
-      // 點選第一個case
-    //   .waitForElementPresent('.checkCasePress',1000,'點選第一個case')
-    //   .pause(1000)
-    //   .execute(function(data){
-    //       $($(".checkCasePress")[0]).trigger('tap');
-    //   })
-      //點選第一個slide
-    //   .waitForElementPresent('.slideBox',1000,'點選第一個slide')
-    //   .pause(1000)
-    //   .execute(function(data){
-    //       $($("#iScrollOverViewSpecimens .slideBox")[0]).trigger('tap');
-    //   })
-      //.perform(function(browser,done){
-      //   console.log('測試結束');
-      //})
-      //.end();
-      //.waitForElementVisible('button[name=btnG]', 1000)
-  },
-  "第二步測試" : function(browser){
-      browser
-      .waitForElementVisible('#firstUL>li:first-child',100000 ,'選擇第一筆Case')
-      .execute(function(data){
-          $('#firstUL>li:first-child').trigger('tap');
-      })
+	"第一步測試": function(browser) {
+		browser
+			.resizeWindow(1920, 1080)
+			.saveScreenshot('./results/test.png')
+			// 開啟視窗
+			.url("https://microscope.paragon.dlcscpth.com/")
+			.waitForElementVisible('input[placeholder=Username]', 10000, '成功開啟視窗')
+			.setValue('input[placeholder=Username]', 'CLOUDPATH')
+			.setValue('input[placeholder=Password]', 'Cloud1111')
+			.execute(function(data) {
+				$('#loginBtn').trigger('touchend');
+			})
+			.waitForElementVisible('.name.ellipsis', 10000, '成功登入');
+			//.assert.visible(".name.ellipsis")
+			//.assert.containsText('.name.ellipsis', 'Cloud Pathologist');
 
-      .waitForElementVisible('#slideIScroll_A ul li div.slide:first-child',100000 ,'選擇第一個slide')
-      .execute(function(data){
-          $('#slideIScroll_A ul li div.slide:first-child').trigger('tap');
-      })
-      .waitForElementVisible('#next',5000,'下一個Case')
-      .execute(function(data){
-          $('#next').trigger('tap');
-      })
+	},
+	"第二步測試": function(browser) {
 
-      .pause(15000)
-      .end();
-  }
+		browser
+			.waitForElementVisible('#firstUL>li:first-child', 10000, '測試開始')
+			.execute(function(data) {
+				$('#firstUL>li:first-child').trigger('tap');
+			});
+
+			var count = 0;
+			var self = this;
+
+			function tapSlide (){
+				count ++;
+				browser
+					.pause(2000)
+					.waitForElementVisible('#slideIScroll_A div.slide:first-child', 50000)
+					//.assert.visible("#slideIScroll_A")
+					.execute(function() {
+						$('#slideIScroll_A ul li div.slide:first-child').trigger('tap');
+					})
+					.waitForElementVisible('#next', 10000)
+					//.waitForElementVisible('#next', 10000, '下一個Case')
+					//.assert.visible("#next")
+					.execute(function() {
+						$('#next').trigger('touchend');
+						return true;
+					},function(result){
+						//console.log("result",result);
+						if(count<100){
+							console.log("點擊次數",count);
+							tapSlide();
+						}else{
+							console.log("點擊測試結束");
+							browser.pause(3000)
+							.end();
+						}
+					});
+				}
+
+			tapSlide();
+
+
+	}
 };
 
+/*
+
+"Drag and drop test (useCss)" : function (browser) {
+  browser
+    .useCss()
+    .moveToElement('#draggable',  0,  0)
+    .mouseButtonDown(0)
+    .moveToElement('body',  200,  600) // Move to offset position of 200(x) 600(y)
+    .mouseButtonUp(0)
+    .pause(5000)  // Keep browser open for 5 seconds so you can see result
+    .end();
+}
+
+*/
 
 /* 範例
 
